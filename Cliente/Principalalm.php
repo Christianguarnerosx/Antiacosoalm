0f43967210dd810185c23c2330ad8f6db80bb487
Christianguarnerosx <christianguarnerosx@gmail.com>
2024-02-23 23:11:20 -0600

    Octavo commit

--- Cliente/Principalalm.php
+++ Cliente/Principalalm.php
@@ -20,58 +20,72 @@
 session_start();
 ?>
 
-<body>
+<body class="scroll-no">
     <div class="container-fluid">
         <div class="row">
             <!--La clase collapse propia de bootstrap 5, utilizada para poder ocultar/contraer columnas y hacer el efecto de menu-->
-            <div class="col-2 collapse" id="colmenuprincipal"> <!--Barra menu de la izq. Principalmente esta colapsada (no se ve)    TAMBIEN se utiliza el tama;o de col por que si no no obtiene el tamano que se le asigan en el css-->
+            <div class="col-2 collapse" id="colmenuprincipalalm"> <!--Barra menu de la izq. Principalmente esta colapsada (no se ve)    TAMBIEN se utiliza el tama;o de col por que si no no obtiene el tamano que se le asigan en el css-->
                 <?php
                 include("include/Menualm.php");
                 ?>
             </div>
 
             <!--columna utilizada para el contenido principal de la interfaz (no opciones lo de en medio) aqui van los botones que abren las demas columnas (menu/configuraciones)-->
-            <div class="col centrar" id="contenedor-principal">
+            <div class="col centrar" id="principalalm-contenedor">
                 <!--Botones que activan menus/extras sobre el contenedro principal porque si se ponen en las columnas obvio no aparecerian -->
-                <button type="button" id="btn-menu-principal" class="btn-transparente" data-bs-toggle="collapse" data-bs-target="#colmenuprincipal" aria-expanded="false" aria-controls="colmenuprincipal">
+                <button type="button" id="btn-menu-principalalm" class="btn-transparente" data-bs-toggle="collapse" data-bs-target="#colmenuprincipalalm" aria-expanded="false" aria-controls="colmenuprincipalalm">
                     <img src="imagenes/iconos/menudesactivado.png" class="icono-m rotar-90" alt="">
                 </button>
-                <button type="button" id="btn-extras-principal" class="btn-transparente" data-bs-toggle="collapse" data-bs-target="#colconfigprincipal" aria-expanded="false" aria-controls="colconfigprincipal">
+                <button type="button" id="btn-extras-principalalm" class="btn-transparente" data-bs-toggle="collapse" data-bs-target="#colconfigprincipalalm" aria-expanded="false" aria-controls="colconfigprincipalalm">
                     <img src="imagenes/avatares/avatar-bmo.gif" class="icono-g" alt="">
                 </button>
 
                 <!--1 contenedor principal ya el contenido de enmedio/principal de la interfaz-->
-                <main class="cuadro-f blanco-transparente">
-                    <img id="principal-logo" class="logo-c" src="imagenes/logos/logoaliadoshorizontal.gif" alt=""> <!--Logo del contenedor de contenido de enmedio-->
-                    <section class="espacio-top-vh">
-                        <!--Fila contenido-->
+                <section id="principalalm-cuadro" class="cuadro-fondo blanco-transparente"> <!--El id se ha utilizado para poner su position relative y asi sea refremcia de otros elementos-->
+                    <!--Logo del contenedor de contenido de enmedio-->
+                    <img id="principalalm-logo" class="logo-c" src="imagenes/logos/logoaliadoshorizontal.gif" alt="">
+                    <!--Contenedor principal (central) de todos los componentes de la interfaz-->
+                    <main id="principalalm-main" class="espacio-top-vh espacio-left-g">
                         <div class="row">
-                            <div class="col espacio-left-g">
-                                <h1 class="titulo-c"><!--Mensaje de bienvenida con nombre. Aqui se obtiene el nombre haciendo referencia a la variable sesion (que tiene el id (obtenido en autenticar)) y consultando en el php de abajo -->
-                                    Hola<?Php include("../Servidor/funciones_session/session_nombreapa.php") ?>: <!--Mandamos a trear una funcion que obtienen el nombre de la session iniciada (Para esto se debe corroborar si se inicio la session_start en este documento)-->
-                                    <h2 class="text-g">¿En qué puedo ser tu aliado de hoy?</h2>
-                                </h1>
+                            <div class="col">
+                                <!--Fila/seccion de titulos y subtitulos-->
+                                <div class="row">
+                                    <h1 class="titulo-c"><!--Mensaje de bienvenida con nombre. Aqui se obtiene el nombre haciendo referencia a la variable sesion (que tiene el id (obtenido en autenticar)) y consultando en el php de abajo -->
+                                        Hola<?Php include("../Servidor/funciones_session/session_nombreapa.php") ?>: <!--Mandamos a trear una funcion que obtienen el nombre de la session iniciada (Para esto se debe corroborar si se inicio la session_start en este documento)-->
+                                        <h2 class="text-g">¿En qué puedo ser tu aliado de hoy?</h2>
+                                    </h1>
+                                </div>
+
+                                <!--fila/seccion de cards de opciones rapidas-->
+                                <div class="row espacio-top-c">
+                                    <!--Tarjeta de opciones de inicio/ en inetrfaz principal-->
+                                    <a class="card-principalalm blanco-solido espacio-left-c decoracion-no"> <!-- Esta Es la tarjeta tipo a, para que sea un link cuando le den click-->
+                                        <div class="row espacio-top-m"><!--Fila de texto de la card-->
+                                            <h1 class="text-m txt-negro espacio-left-c">Comunidad</h1>
+                                            <p class="text-c txt-negro espacio-left-c">Reportes de aliado</p>
+                                        </div>
+                                        <div class="row"> <!-- Fila de el icono inferior en la card -->
+                                            <img class="icono-card-rb" src="imagenes/iconos/iconoplay.png" alt="">
+                                        </div>
+                                    </a>
+                                </div>
+
+                                <div id="principalalm-barra-busqueda" class="row centrar-v">
+                                    <div class="col">
+                                        <input class="form-control text-m" id="" type="text" placeholder="Escribe lo que estas buscando" aria-label="">
+                                    </div>
+                                    <div class="col-1">
+                                        <h1>B</h1>
+                                    </div>
+                                </div>
                             </div>
                         </div>
-                        <div class="row espacio-top-m espacio-left-g">
-                            <a class="card-g blanco-solido centrar espacio-left-c es decoracion-no txt-negro">
-                                <h1 class=" text-m">Hola</h1>
-                            </a>
-
-                            <a class="card-g blanco-solido centrar espacio-left-c decoracion-no txt-negro">
-                                <h1>Hola</h1>
-                            </a>
-
-                            <a class="card-g blanco-solido centrar espacio-left-c decoracion-no txt-negro">
-                                <h1>Hola</h1>
-                            </a>
-                        </div>
-                    </section>
-                </main>
+                    </main>
+                </section>
             </div>
 
             <!--Columna de configuracion se abre dando click al avatar, aqui se manda a traer las opciones posibkes a configuar-->
-            <div class="col collapse" id="colconfigprincipal"> <!--Barra menu de la izq-->
+            <div class="col collapse" id="colconfigprincipalalm"> <!--Barra menu de la izq-->
 
             </div>
         </div>
--- Cliente/js/logica_menu_principal.js
+++ Cliente/js/logica_menu_principal.js
@@ -1,36 +1,37 @@
 /*Logica para que funcione el boton menu*/
 /*Hace que ela columna que esta en collapse aparezca al agregarle el action de bootstrap shown.bs.collapse (Asi aparecera la columa del menu)*/
-document.getElementById("colmenuprincipal").addEventListener("show.bs.collapse", function () {
-    document.getElementById("colmenuprincipal").style.width = "300px";/*Posicion que se tomara la col mneu a lo ancho cuando este sea dezplegado*/
-    document.getElementById("btn-menu-principal").style.top = "-10px"; /*Posicion que tomara a lo alto cuando el menu este dezplegado*/
-    document.getElementById("btn-menu-principal").style.left = "-13px"; /*Posicion que tomara a la izq cuando el menu este dezplegado*/
-    document.getElementById("btn-menu-principal").style.rotate = "-90deg"; /* Utilizado para dar la a animacionde girar*/
-    document.getElementById("btn-menu-principal").style.scale = "0.7"; /*Hara que el boton se haga mas pequeno*/
+document.getElementById("colmenuprincipalalm").addEventListener("show.bs.collapse", function () {
+    document.getElementById("colmenuprincipalalm").style.width = "300px";/*Posicion que se tomara la col mneu a lo ancho cuando este sea dezplegado*/
+    document.getElementById("btn-menu-principalalm").style.top = "-10px"; /*Posicion que tomara a lo alto cuando el menu este dezplegado*/
+    document.getElementById("btn-menu-principalalm").style.left = "-13px"; /*Posicion que tomara a la izq cuando el menu este dezplegado*/
+    document.getElementById("btn-menu-principalalm").style.rotate = "-90deg"; /* Utilizado para dar la a animacionde girar*/
+    document.getElementById("btn-menu-principalalm").style.scale = "0.7"; /*Hara que el boton se haga mas pequeno*/
+    document.getElementById("btn-menu-principalalm").style.scale = "0.7"; /*Hara que el boton se haga mas pequeno*/
 });
 
 /*De la misma manera se aplica el event de hidden a la col que se quiera ocultar al agregarle el hide.bs,collapse*/
-document.getElementById("colmenuprincipal").addEventListener("hide.bs.collapse", function () {
-    document.getElementById("btn-menu-principal").style.top = "calc(45vh - 30px)"; /*Esta es la medida que tiene en el css (Con la que comienza) Regresar a la misma posicion*/
-    document.getElementById("btn-menu-principal").style.rotate = "0deg"; /*Utilizado para dar la a animacionde girar*/
-    document.getElementById("btn-menu-principal").style.left = "25px"; /*Esta es la medida que tiene en el css (Con la que comienza) Regresar a la misma posicion*/
-    document.getElementById("btn-menu-principal").style.scale = "1"; /*Esta es la medida que tiene en el css (Con la que comienza) Regresar al mismo tamano*/
+document.getElementById("colmenuprincipalalm").addEventListener("hide.bs.collapse", function () {
+    document.getElementById("btn-menu-principalalm").style.top = "calc(45vh - 30px)"; /*Esta es la medida que tiene en el css (Con la que comienza) Regresar a la misma posicion*/
+    document.getElementById("btn-menu-principalalm").style.rotate = "0deg"; /*Utilizado para dar la a animacionde girar*/
+    document.getElementById("btn-menu-principalalm").style.left = "25px"; /*Esta es la medida que tiene en el css (Con la que comienza) Regresar a la misma posicion*/
+    document.getElementById("btn-menu-principalalm").style.scale = "1"; /*Esta es la medida que tiene en el css (Con la que comienza) Regresar al mismo tamano*/
 });
 
 /*Logica para que funcione el boton extras/opciones*/
 /*Hace que ela columna que esta en collapse aparezca al agregarle el action de bootstrap shown.bs.collapse (Asi aparecera la columa del menu)*/
-document.getElementById("colconfigprincipal").addEventListener("show.bs.collapse", function () {
-    document.getElementById("colconfigprincipal").style.zIndex = "1";/*Este ahce que se sobreponga a todo lo demas del contenido es como en canva/word "Traer/mandar adelante de todo (lo que este debajo de "1")*/
-    document.getElementById("btn-menu-principal").style.zIndex = "0";/*Este ahce que se sobreponga a todo lo demas del contenido es como en canva/word "Traer/mandar adelante de todo (lo que este debajo de "1")*/
-    document.getElementById("btn-extras-principal").style.scale = "3"; /*Hara que el boton se haga mas pequeno*/
-    document.getElementById("btn-extras-principal").style.right = "45%";/*Posicion que tomara a la derecha cuando el menu este dezplegado*/
-    document.getElementById("btn-extras-principal").style.top = "200px";/*Posicion que tomara a lo alto cuando el menu este dezplegado*/
-    document.getElementById("btn-extras-principal").style.rotate = "360deg"; /* Utilizado para dar la a animacionde girar*/
+document.getElementById("colconfigprincipalalm").addEventListener("show.bs.collapse", function () {
+    document.getElementById("colconfigprincipalalm").style.zIndex = "1";/*Este ahce que se sobreponga a todo lo demas del contenido es como en canva/word "Traer/mandar adelante de todo (lo que este debajo de "1")*/
+    document.getElementById("btn-menu-principalalm").style.zIndex = "0";/*Este ahce que se sobreponga a todo lo demas del contenido es como en canva/word "Traer/mandar adelante de todo (lo que este debajo de "1")*/
+    document.getElementById("btn-extras-principalalm").style.scale = "3"; /*Hara que el boton se haga mas pequeno*/
+    document.getElementById("btn-extras-principalalm").style.right = "45%";/*Posicion que tomara a la derecha cuando el menu este dezplegado*/
+    document.getElementById("btn-extras-principalalm").style.top = "200px";/*Posicion que tomara a lo alto cuando el menu este dezplegado*/
+    document.getElementById("btn-extras-principalalm").style.rotate = "360deg"; /* Utilizado para dar la a animacionde girar*/
 });
 
 /*De la misma manera se aplica el event de hidden a la col que se quiera ocultar al agregarle el hide.bs,collapse*/
-document.getElementById("colconfigprincipal").addEventListener("hide.bs.collapse", function () {
-    document.getElementById("btn-extras-principal").style.scale = "1"; /*Este ahce que se sobreponga a todo lo demas del contenido es como en canva/word "Traer/mandar adelante de todo (lo que este debajo de "1")*/
-    document.getElementById("btn-extras-principal").style.right = "25px";/*Esta es la medida que tiene en el css (Con la que comienza) Regresar a la misma posicion*/
-    document.getElementById("btn-extras-principal").style.top = "25px";/*Esta es la medida que tiene en el css (Con la que comienza) Regresar a la misma posicion*/
-    document.getElementById("btn-extras-principal").style.rotate = "0deg"; /* Utilizado para dar la a animacionde girar*/
+document.getElementById("colconfigprincipalalm").addEventListener("hide.bs.collapse", function () {
+    document.getElementById("btn-extras-principalalm").style.scale = "1"; /*Este ahce que se sobreponga a todo lo demas del contenido es como en canva/word "Traer/mandar adelante de todo (lo que este debajo de "1")*/
+    document.getElementById("btn-extras-principalalm").style.right = "25px";/*Esta es la medida que tiene en el css (Con la que comienza) Regresar a la misma posicion*/
+    document.getElementById("btn-extras-principalalm").style.top = "25px";/*Esta es la medida que tiene en el css (Con la que comienza) Regresar a la misma posicion*/
+    document.getElementById("btn-extras-principalalm").style.rotate = "0deg"; /* Utilizado para dar la a animacionde girar*/
 });
